b6878ff1902cbc1326b123c628c97c8e12dd1520
Süheyla CAN <suheylacan@Suheyla-MacBook-Air.local>
2026-01-30 15:21:10 +0300

    guncellendi

---
+++ models/Notes.js
@@ -0,0 +1,17 @@
+
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model("Notes", noteSchema);
--- not_app.js
+++ not_app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -5,44 +6,59 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "not_views");
 
-app.listen(8000);
+app.listen(8000, () => {
+    console.log("Listening requests on port 8000...")
+});
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended:true}));
+
+const connectDB = require("./config/db");
+connectDB();
+
+const Note = require("./models/Notes");
 
 app.get("/", (req,res) => {
-    const notes = [
-        {title: "Shopping List", content: "tomato, cucumber, soap, detergent, milk"},
-        {title: "Exam Week", content: `
-            Monday: Data Structures and Algorithms ,
-            Tuesday: Discrete Computational Structures ,
-            Wendnesday: Electrical Circuits ,
-            Thursday: Basic Lineer Algebra ,
-            Friday: Differential Equations
-        `}
-    ];
-    res.render("not_index", {title: "Home", notes});
+        Note.find().sort({createdAt: -1})
+        .then(notes => {
+            res.render("not_index", {
+                title: "Home",
+                notes
+            });
+        });
 });
 
 app.get("/about", (req,res) => {
     res.render("not_about", {title: "About Page"});
 });
 
-app.get("/create-notes", (req,res) => {
-    res.render("not_create", {title: "Create a New Note"});
+app.get("/create-notes", (req, res) => {
+    res.render("not_create", { title: "Create Notes" });
+});
+
+app.post("/create-notes", (req,res) => {
+    const note = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+    note.save()
+    .then(() => res.redirect("/all-notes"))
+    .catch(err => console.log(err));
 });
 
 app.get("/all-notes", (req,res) => {
-    const notes = [
-        {title: "Shopping List", content: "tomato, cucumber, soap, detergent, milk"},
-        {title: "Exam Week", content: `
-            Monday: Data Structures and Algorithms ,
-            Tuesday: Discrete Computational Structures ,
-            Wendnesday: Electrical Circuits ,
-            Thursday: Basic Lineer Algebra ,
-            Friday: Differential Equations
-        `}
-    ];
-    res.render("not_all", {title: "All Notes" , notes});
+
+        Note.find().sort({createdAt: -1})
+        .then(notes => {
+            res.render("not_all", {
+                title: "All Notes",
+                notes
+            });
+        })
+
 });
 
-app.get((req,res) => {
+app.use((req,res) => {
     res.status(404).render("not_error", {title: "404"});
 });
